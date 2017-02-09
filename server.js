44782b1a829b726da603589514aae24c3bc2e183
Alburtus <palburtus@03-c02qp1sng8wm.webmdhealth.net>
2017-02-09 16:32:44 -0500

    Inital commit of source files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(3000, function () {
+    console.log('express-handlebars example server listening on: 3000');
+});
